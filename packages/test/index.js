29fab1f03ef840eb271943a5aeaf9b5ce77654d3
Johnny Richardson <johnny.c.rich@gmail.com>
2020-04-17 11:04:14 -0400

    refactor(test): update module export to reflect use of KS graphql app.

--- packages/test/index.js
+++ packages/test/index.js
@@ -22,37 +22,38 @@ const models = require('./models');
 
 module.exports = (routesImporter) => {
 
-    const dataFile = fs.readFileSync(`${__dirname}/config.json`);
-    const configData = JSON.parse(dataFile);
-    const pkgRouter = routes(routesImporter);
+  const dataFile = fs.readFileSync(`${__dirname}/config.json`);
+  const configData = JSON.parse(dataFile);
+  const pkgRouter = routes(routesImporter);
 
-    const dbAddress =
-      process.env.NODE_ENV === 'development' ?
-      `mongodb://localhost/${configData.database}` :
-      `${process.env.MONGO_CLOUD_URI}${configData.database}?retryWrites=true&w=majority`;
+  const dbAddress =
+    process.env.NODE_ENV === 'development' ?
+    `mongodb://localhost/${configData.database}` :
+    `${process.env.MONGO_CLOUD_URI}${configData.database}?retryWrites=true&w=majority`;
 
-    const keystone = new Keystone({
-      adapter: new MongooseAdapter({
-        mongoUri: dbAddress
-      })
+  const keystone = new Keystone({
+    adapter: new MongooseAdapter({
+      mongoUri: dbAddress
+    })
+  });
+
+  keystone
+    .prepare({
+      apps: [new GraphQLApp()]
+    })
+    .then(() => {
+      keystone.connect();
+      // Tell package to use this graphql app for all queries
+      pkgRouter.use((req, res, next) => {
+        res.locals.db = keystone;
+        next();
+      });
     });
 
-    keystone
-      .prepare({
-        apps: [new GraphQLApp()]
-      })
-      .then(() => {
-        keystone.connect();
-        // Tell package to use this graphql app for all queries
-        pkgRouter.use((req, res, next) => {
-          res.locals.db = keystone;
-          next();
-        });
-      });
-
-    // TODO: give all routes a namespace prefix, e.g. 'homepage/'
-    return {
-      Routes: pkgRouter,
-      Models: models,
-      Config: configData
-    };
+  // TODO: give all routes a namespace prefix, e.g. 'homepage/'
+  return {
+    Routes: pkgRouter,
+    Models: models,
+    Config: configData
+  }
+};
