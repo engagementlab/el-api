8dc00eb147c0443625a381da8c6215567f666fb3
Ralph Drake <rama.o.ralph@gmail.com>
2020-04-24 16:18:03 -0400

    fix: Attempted fix for promise resolution error from require()
    - Move the `new Promise` declaration directly to the returned value.
    - Pending investigation on Linux
    - May have something to do with defining an async function in a Promise

--- packages/engagement-lab-home/index.js
+++ packages/engagement-lab-home/index.js
@@ -15,30 +15,28 @@ const fs = require('fs');
 const routes = require('./routes');
 const models = require('./models')();
 
-module.exports = routesImporter => {
-  return new Promise(async resolve => {
-    const dataFile = fs.readFileSync(`${__dirname}/config.json`);
-    const configData = JSON.parse(dataFile);
+module.exports = (routesImporter) => new Promise(async resolve => {
+  const dataFile = fs.readFileSync(`${__dirname}/config.json`);
+  const configData = JSON.parse(dataFile);
 
-    const dbAddress =
-      process.env.NODE_ENV === 'development' ?
+  const dbAddress =
+    process.env.NODE_ENV === 'development' ?
       'mongodb://localhost' :
       `${process.env.MONGO_CLOUD_URI}${configData.database}?retryWrites=true&w=majority`;
 
-    const client = await MongoClient.connect(dbAddress, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    const db = client.db(configData.database)
-
-    const appRoutes = routes(routesImporter, db);
-    global.logger.info('🚀 Homepage API ready.');
-
-    // TODO: give all routes a namespace prefix, e.g. 'homepage/'
-    resolve({
-      Routes: appRoutes,
-      Models: models,
-      Config: configData
-    });
+  const client = await MongoClient.connect(dbAddress, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   });
-};
+  const db = client.db(configData.database)
+
+  const appRoutes = routes(routesImporter, db);
+  global.logger.info('🚀 Homepage API ready.');
+
+  // TODO: give all routes a namespace prefix, e.g. 'homepage/'
+  resolve({
+    Routes: appRoutes,
+    Models: models,
+    Config: configData
+  });
+});
