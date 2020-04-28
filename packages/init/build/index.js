4d15a648039f277ac39265072e4f057fb49209e5
Johnny Richardson <johnny.c.rich@gmail.com>
2020-04-28 17:47:12 -0400

    feat(build): Retrieve all packages to build from packages/ dir dynamically, not from json.

--- packages/init/build/index.js
+++ packages/init/build/index.js
@@ -27,50 +27,68 @@ const appsJson = path.join(__dirname, '../apps.json');
  * Get config data for all sibling app packages, or for one if name specified.
  * @function
  * @param {boolean} list - Just list app packages
- * @param {string} pkgName - Name of app for package info to load
+ * @param {string} [pkgNames] - Names of apps for package info to return (comma-seperated)
  */
-const getPackagesData = (list, pkgName) => {
-
+const getPackagesData = (list, pkgNames) => {
     const pkgsPath = path.join(__dirname, '../../../packages');
-    fs.readdir(pkgsPath, (err, dirs) => {
+    const dirs = fs.readdirSync(pkgsPath);
+    let namesStr = '';
+    const namesObj = {};
+    // Do not include 'init' package, and only package(s) specified if any
+    const dirsFiltered = dirs.filter(name => name !== 'init');
+    if (pkgNames) {
+        const pkgArr = pkgNames.split(',');
+        dirsFiltered.filter(name => pkgArr.indexOf(name) > -1);
+    }
 
-        let names = '';
-        dirs.filter((name) =>
-            name !== 'init').forEach(name => {
-            if (fs.statSync(path.join(pkgsPath, name)).isDirectory())
-                names += `\n ${name}`;
-        });
-
-        if (list)
-            global.logger.info(names);
-
+    dirsFiltered.forEach(name => {
+        if (fs.statSync(path.join(pkgsPath, name)).isDirectory()) {
+            // Get formal app name
+            const configData = JSON.parse(
+                fs.readFileSync(path.join(pkgsPath, name, 'config.json'))
+            );
+            // Obj for usage in build gen
+            namesObj[configData.schema] = configData.name;
+            namesStr += `\n  🔸 ${colors.bold(configData.name)} (${colors.yellow(
+        name
+      )})`;
+        }
     });
+
+    if (list) {
+        global.logger.info(
+            `\n\n📦 ${colors.green.underline(
+        'Packages found for build:'
+      )} ${namesStr}`
+        );
+        return {};
+    }
+
+    return namesObj;
 };
 
 /**
- *
+ * Send either all of specified app packages to CMS front-end generator.
  * @module
- * @param {function}
  */
 module.exports = (() => {
+    let packages = [];
 
     /* 
-     Handle app arguments
-     */
+      Handle app arguments
+      */
     // List all package names in repo
     if (argv.list) {
         getPackagesData(true);
-        return
+        return;
     }
-
-
-    // Load all possible apps from sibling packages (config defined in app.json)
-    const appConfigs = fs.readFileSync(appsJson);
-    const packages = JSON.parse(appConfigs);
+    // Retrieve info and build only packages specified, or all if not
+    packages = getPackagesData(false, argv.packages);
 
     // For all packages...
-    packages.forEach(async pkg => {
-
+    const keys = Object.keys(packages);
+    keys.forEach(async pkgKey => {
+        const pkg = packages[pkgKey];
         try {
             // Call CMS distributable static generator for the current package
             const child = spawn('npm', [
@@ -78,27 +96,27 @@ module.exports = (() => {
                 'build',
                 '--',
                 `--entry=${__dirname}/generator.js`,
-                `--out=./../../bin/${pkg}`,
-                `--app=${pkg}`,
-                `--allApps=${packages}`
+                `--out=./../../bin/${pkgKey}`,
+                `--app=${pkgKey}`,
+                `--allApps=${keys}`
             ]);
 
-            child.stdout.on('data', (chunk) => {
+            child.stdout.on('data', chunk => {
                 global.logger.info(chunk);
             });
 
-            child.stdout.on('err', (chunk) => {
+            child.on('error', chunk => {
                 global.logger.error(chunk);
             });
 
-            child.stdout.on('close', () => {
-                global.logger.info(`🍺 CMS bundle exported for ${colors.yellow(pkg)}.`);
-
+            child.on('exit', (err, info) => {
+                if (err !== 0)
+                    global.logger.error(`⛔ Uncaught error for ${colors.yellow(pkg)} process.`);
+                else
+                    global.logger.info(`🍺 CMS bundle exported for ${colors.yellow(pkg)}.`);
             });
-
         } catch (err) {
             global.logger.error(err);
         }
     });
-
 })();
