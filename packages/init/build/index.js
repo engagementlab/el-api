3418b6b1f3ffe7315057530421dbbd9ef6fe5eef
Johnny Richardson <johnny.c.rich@gmail.com>
2020-04-30 13:57:05 -0400

    refactor(build): update post-build commands to further cleanup bin/ dirs.

--- packages/init/build/index.js
+++ packages/init/build/index.js
@@ -9,10 +9,15 @@
 
 const fs = require('fs');
 const path = require('path');
-const { spawn } = require('child_process');
-const { argv } = require('yargs');
+const {
+    spawn
+} = require('child_process');
+const {
+    argv
+} = require('yargs');
 
 const colors = require('colors');
+const glob = require('glob');
 
 // Build utils
 const utils = require('./utils')();
@@ -27,19 +32,44 @@ require('../logger');
  *
  */
 const postBuild = paths => {
-  paths.forEach(buildPath => {
-    const binPath = path.join(__dirname, '..', buildPath);
-    // Ensure is directory
-    if (fs.statSync(binPath).isDirectory()) {
-      const indexPathOg = path.join(binPath, 'admin/secure/index.html');
-      const indexPathNew = path.join(binPath, 'index.html');
-      const jsPathOg = path.join(binPath, 'admin/secure/js');
-      const jsPathNew = path.join(binPath, 'admin/js');
+    paths.forEach(buildPath => {
+        const binPath = path.join(__dirname, '..', buildPath);
+        // Ensure is directory
+        if (fs.statSync(binPath).isDirectory()) {
+            const jsPath = path.join(binPath, 'admin/secure/js');
+            const bundleFile = glob.sync('main*.js', {
+                cwd: jsPath
+            })[0];
+            const listFiles = glob.sync('*bundle.js', {
+                cwd: jsPath
+            });
 
-      fs.renameSync(indexPathOg, indexPathNew);
-      fs.renameSync(jsPathOg, jsPathNew);
-    }
-  });
+            try {
+                // Delete index; it is served by views/cms template
+                fs.unlinkSync(path.join(binPath, 'admin/secure/index.html'));
+                //  Move 'main' webpack bundle as [root]/bundle.js
+                fs.renameSync(
+                    path.join(jsPath, bundleFile),
+                    path.join(binPath, 'bundle.js')
+                );
+                // Move all bundles per CMS list under [root]/js
+                fs.mkdirSync(path.join(binPath, 'js'));
+                listFiles.forEach(file => {
+                    fs.renameSync(
+                        path.join(jsPath, file),
+                        path.join(binPath, `js/${file}`)
+                    );
+                });
+                // Delete dangling dir
+                fs.rmdir(path.join(binPath, 'admin/secure/js'), {}, (err) => {
+                    console.error(err);
+
+                });
+            } catch (e) {
+                throw new Error(e);
+            }
+        }
+    });
 };
 
 /**
@@ -47,68 +77,68 @@ const postBuild = paths => {
  * @module
  */
 module.exports = (() => {
-  let packages = [];
+    let packages = [];
 
-  /* 
-      Handle app arguments
-      */
-  // List all package names in repo
-  if (argv.list) {
-    utils.GetPackagesData(true);
-    return;
-  }
-  // Retrieve info and build only packages specified, or all if not
-  packages = utils.GetPackagesData(false, argv.packages);
-
-  // For all packages...
-  const keys = Object.keys(packages);
-  // Track exported builds done via their build folders, for post-build scripting
-  const exportsDone = [];
-
-  keys.forEach(async pkgKey => {
-    const pkg = packages[pkgKey];
-    const outDir = `./../../bin/${pkgKey}`;
-    try {
-      // Call CMS distributable static generator for the current package
-      const child = spawn('npm', [
-        'run',
-        'build',
-        '--',
-        `--entry=${__dirname}/generator.js`,
-        `--out=${outDir}`,
-        `--app=${pkgKey}`,
-        `--allApps=${keys}`
-      ]);
-
-      child.stdout.on('data', chunk => {
-        global.logger.info(chunk);
-      });
-
-      child.on('error', chunk => {
-        global.logger.error(chunk);
-      });
-
-      child.on('exit', (err, info) => {
-        if (err !== 0)
-          global.logger.error(
-            `⛔ Uncaught error for ${colors.yellow(pkg.name)} process.`
-          );
-        else {
-          global.logger.info(
-            `🍺 CMS bundle exported for ${colors.yellow(pkg.name)}.`
-          );
-          exportsDone.push(outDir);
-
-          if (exportsDone.length === keys.length) {
-            postBuild(exportsDone);
-            global.logger.info(
-              `✨  All bundles done, organizing directories.  ✨`
-            );
-          }
-        }
-      });
-    } catch (err) {
-      global.logger.error(err);
+    /* 
+          Handle app arguments
+          */
+    // List all package names in repo
+    if (argv.list) {
+        utils.GetPackagesData(true);
+        return;
     }
-  });
+    // Retrieve info and build only packages specified, or all if not
+    packages = utils.GetPackagesData(false, argv.packages);
+
+    // For all packages...
+    const keys = Object.keys(packages);
+    // Track exported builds done via their build folders, for post-build scripting
+    const exportsDone = [];
+
+    keys.forEach(async pkgKey => {
+        const pkg = packages[pkgKey];
+        const outDir = `./../../bin/${pkgKey}`;
+        try {
+            // Call CMS distributable static generator for the current package
+            const child = spawn('npm', [
+                'run',
+                'build',
+                '--',
+                `--entry=${__dirname}/generator.js`,
+                `--out=${outDir}`,
+                `--app=${pkgKey}`,
+                `--allApps=${keys}`
+            ]);
+
+            child.stdout.on('data', chunk => {
+                global.logger.info(chunk);
+            });
+
+            child.on('error', chunk => {
+                global.logger.error(chunk);
+            });
+
+            child.on('exit', (err, info) => {
+                if (err !== 0)
+                    global.logger.error(
+                        `⛔ Uncaught error for ${colors.yellow(pkg.name)} process.`
+                    );
+                else {
+                    global.logger.info(
+                        `🍺 CMS bundle exported for ${colors.yellow(pkg.name)}.`
+                    );
+                    exportsDone.push(outDir);
+
+                    if (exportsDone.length === keys.length) {
+                        global.logger.info(
+                            `✨  All bundles done, organizing directories.  ✨`
+                        );
+                        postBuild(exportsDone);
+                    }
+                }
+            });
+        } catch (err) {
+            global.logger.error(err);
+        }
+    });
 })();
