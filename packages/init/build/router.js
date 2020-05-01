91398049a74cbca518473a43e359f4c0243df092
Johnny Richardson <johnny.c.rich@gmail.com>
2020-05-01 18:02:01 -0400

    feat(init): more work on authentication.

--- packages/init/build/router.js
+++ packages/init/build/router.js
@@ -9,6 +9,7 @@
 
 const fs = require('fs');
 const express = require('express');
+const session = require('express-session');
 
 // Passport
 const passport = require('passport');
@@ -31,25 +32,34 @@ const authentication = {
         // if(process.env.NODE_ENV === 'development') {
         // 	res.redirect('/cms');
         // 	return;
-        // } 
+        // }
+        console.trace();
 
         passport.authenticate('google', (err, user, info) => {
-            console.log(req);
+            console.log(user);
             if (err) {
-                global.logger.error('Auth error!', err);
+                // global.logger.error('Auth error:', err);
                 return next(err);
             }
             if (!user) {
                 global.logger.error('No user!', info);
                 return res.redirect('/');
             }
-            req.logIn(user, (err) => {
+            req.logIn(user, err => {
+                console.log('login');
                 if (err) {
                     return next(err);
                 }
-                return res.redirect('/cms/home');
+                return res.redirect(req.originalUrl || '/cms/@/home');
             });
         })(req, res, next);
+    },
+
+    isAllowed: (req, res, next) => {
+        console.log('authed', req.isAuthenticated());
+
+        if (req.isAuthenticated()) next();
+        else res.redirect('/');
     }
 };
 
@@ -77,6 +87,16 @@ module.exports = buildsDir => {
      * Google oauth2/passport config
      */
 
+    // Session store
+    //  TODO: mongostore for prod
+    router.use(
+        session({
+            secret: process.env.SESSION_COOKIE,
+            resave: true,
+            saveUninitialized: false
+        })
+    );
+
     router.use(passport.initialize());
     router.use(passport.session());
 
@@ -84,23 +104,28 @@ module.exports = buildsDir => {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: process.env.AUTH_CALLBACK_URL,
-            passReqToCallback: true,
+            passReqToCallback: true
         },
         (request, accessToken, refreshToken, profile, done) => {
+            console.log(accessToken, refreshToken, profile);
             // Verify user allowed
             User.findOne({
-                email: profile.emails[0].value
-            }, (err, user) => {
-                if (err) {
-                    global.logger.error(`Login error: ${err}`);
-                    return null;
+                    email: profile.emails[0].value
+                },
+                (err, user) => {
+                    if (err) {
+                        global.logger.error(`Login error: ${err}`);
+                        return null;
+                    }
+                    if (!user) {
+                        global.logger.error(
+                            `Login error: user not found for email ${profile.emails[0].value}`
+                        );
+                        return null;
+                    }
+                    return done(err, user);
                 }
-                if (!user) {
-                    global.logger.error(`Login error: user not found for email ${ profile.emails[0].value}`);
-                    return null;
-                }
-                return done(err, user);
-            });
+            );
         }
     );
     passport.use(strategy);
@@ -108,6 +133,7 @@ module.exports = buildsDir => {
         done(null, user);
     });
     passport.deserializeUser((user, done) => {
+        console.log('DE', user);
         done(null, user);
     });
 
@@ -116,9 +142,11 @@ module.exports = buildsDir => {
 
     // Authentication
     router.get('/callback', authentication.callback);
-    router.get('/*', authentication.login, (req, res, next) => {
-        next();
+    router.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
     });
+    router.get('/*', authentication.login);
 
     // Create route in router for all builds
     allDirs.forEach(name => {
@@ -128,7 +156,7 @@ module.exports = buildsDir => {
         });
 
         // We also need a route to render index for all intermediates as per react-dom-router
-        router.get(`/@/${name}*`, (req, res) => {
+        router.get(`/@/${name}*`, authentication.isAllowed, (req, res) => {
             res.render('cms', {
                 schema: name
             });
@@ -138,14 +166,16 @@ module.exports = buildsDir => {
     // If on dev instance, create dev user if none
     // TODO: Limit to dev
     User.findOne({
-        email: process.env.DEV_EMAIL
-    }, (err, user) => {
-        if (!user) {
-            User.create({
-                email: process.env.DEV_EMAIL,
-                name: 'Dev User'
-            });
+            email: process.env.DEV_EMAIL
+        },
+        (err, user) => {
+            if (!user) {
+                User.create({
+                    email: process.env.DEV_EMAIL,
+                    name: 'Dev User'
+                });
+            }
         }
-    });
+    );
     return router;
 };
