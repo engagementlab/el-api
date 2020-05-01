952a4fdc5094720058cc0a7c9977be10bc82b753
Johnny Richardson <johnny.c.rich@gmail.com>
2020-05-01 15:01:46 -0400

    feat(init): User model for authentication.

---
+++ packages/init/models/User.js
@@ -0,0 +1,41 @@
+/**
+ * Engagement Lab Content and Data API
+ * Developed by Engagement Lab, 2020
+ *
+ * @author Johnny Richardson
+ * User model
+ */
+
+const mongoose = require('mongoose');
+
+const {
+    Schema
+} = mongoose;
+
+const userSchema = new Schema({
+    googleId: {
+        type: String,
+        index: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    permissions: {
+        type: Array,
+    },
+    lastLogin: {
+        type: Date
+    },
+});
+
+/**
+ * Registration
+ */
+module.exports = mongoose.model('User', userSchema);
