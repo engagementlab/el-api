58a209338e02f263c86bd36bdf6f387e1f711b15
Johnny Richardson <johnny.c.rich@gmail.com>
2020-05-11 15:54:10 -0400

    test: added more tests.

--- packages/engagement-lab-home/tests/routes.js
+++ packages/engagement-lab-home/tests/routes.js
@@ -13,22 +13,34 @@ const {
     expect,
 } = require('chai');
 
-describe('Get all route content and ensure JSON response with required keys.', () => {
-    it('About content', done => {
-        request
-            .get('/get/about').end((err, res) => {
-                expect(res.statusCode).to.equal(200);
-                expect(res.body).to.contain.keys(
-                    'missionStatement',
-                    'summary1',
-                    'summary2',
-                    'research',
-                    'workshops',
-                    'tools',
-                    'teaching',
-                    'design'
-                );
-                done(err);
-            });
+describe('Engagement Lab Homepage API', () => {
+    it('Try to get non-existent route.', done => {
+        const route = Math.ceil(Math.random() * 10);
+        request.get(`/get/${route}`).end((err, res) => {
+            expect(res.statusCode).to.equal(500);
+            expect(res.text).contains.a('string', `No route found for path /get/${route}.`);
+
+            done(err);
+        });
+    });
+    describe('Get all route content and ensure JSON response with required keys.', () => {
+
+        it('About content', done => {
+            request
+                .get('/get/about').end((err, res) => {
+                    expect(res.statusCode).to.equal(200);
+                    expect(res.body).to.contain.keys(
+                        'missionStatement',
+                        'summary1',
+                        'summary2',
+                        'research',
+                        'workshops',
+                        'tools',
+                        'teaching',
+                        'design'
+                    );
+                    done(err);
+                });
+        });
     });
 });
--- packages/init/tests/boot.js
+++ packages/init/tests/boot.js
@@ -14,9 +14,33 @@ const {
     expect,
 } = require('chai');
 
-describe('App starts and agent can load index.', () => {
-    it('CMS index is valid html, status 200,', done => {
+describe('App starts and agent can load root.', () => {
+    it('Server booted, status 200,', done => {
         request.get('/').end((err, res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.type).to.be.a('string', 'text/html');
+            done(err);
+        });
+    });
+});
+
+describe('Mock user is logged in.', () => {
+    it('CMS index redirects user to login route,', done => {
+        request.get('/cms').end((err, res) => {
+            expect(res.statusCode).to.equal(302);
+            expect(res.header.location).to.be.a('string', '/cms/login');
+            done(err);
+        });
+    });
+    it('CMS login route redirects to callback,', done => {
+        request.get('/cms/login').end((err, res) => {
+            expect(res.statusCode).to.equal(302);
+            expect(res.header.location).to.contain.a('string', '/callback?__mock_strategy_callback=true');
+            done(err);
+        });
+    });
+    it('CMS mock user is logged in, admin page is valid HTML,', done => {
+        request.get('/cms/admin').end((err, res) => {
             expect(res.statusCode).to.equal(200);
             expect(res.type).to.be.a('string', 'text/html');
             done(err);
