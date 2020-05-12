6fc938150780f92c0b19aeaae1c99ff0f87eba0c
Johnny Richardson <johnny.c.rich@gmail.com>
2020-05-12 18:33:32 -0400

    test(init): more cms tests.

--- packages/init/tests/cms.js
+++ packages/init/tests/cms.js
@@ -22,6 +22,7 @@ describe('Mock user is logged in.', () => {
             done(err);
         });
     });
+
     it('CMS index redirects user to login route,', done => {
         request.get('/cms').end((err, res) => {
             expect(res.statusCode).to.equal(302);
@@ -29,6 +30,7 @@ describe('Mock user is logged in.', () => {
             done(err);
         });
     });
+
     it('CMS mock user is logged in, admin page is valid HTML,', done => {
         request.get('/cms/admin').end((err, res) => {
             expect(res.statusCode).to.equal(200);
@@ -36,6 +38,59 @@ describe('Mock user is logged in.', () => {
             done(err);
         });
     });
+
+    it('CMS mock user creates a new user via admin tool,', done => {
+        const body = {
+            name: 'Admin User',
+            email: 'email@place.org',
+            isAdmin: true,
+            permissions: ['test'],
+        };
+
+        request.post('/cms/admin/edit')
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(body)).end((err, res) => {
+                expect(res).to.not.be.undefined;
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.have.property('msg', 'created');
+
+                done();
+            });
+    });
+
+    it('CMS mock user edits same user via admin tool,', done => {
+        const body = {
+            email: 'email@place.org',
+            isAdmin: false,
+            permissions: ['test', 'test-2'],
+        };
+
+        request.post('/cms/admin/edit')
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(body)).end((err, res) => {
+                expect(res).to.not.be.undefined;
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.have.property('msg', 'saved');
+                done();
+            });
+    });
+
+    it('CMS mock user deletes same user via admin tool,', done => {
+        const body = {
+            email: 'email@place.org',
+            delete: true,
+        };
+
+        request.post('/cms/admin/edit')
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(body)).end((err, res) => {
+                expect(res).to.not.be.undefined;
+                expect(res.statusCode).to.equal(200);
+                expect(res.body).to.have.property('msg', 'deleted');
+                done();
+            });
+    });
+
     it('User tries to load \"engagement-lab-home\" CMS non-React router and is redirected,', done => {
         request.get('/cms/home').end((err, res) => {
             expect(res.statusCode).to.equal(302);
@@ -43,13 +98,14 @@ describe('Mock user is logged in.', () => {
             done(err);
         });
     });
+
     it('User can load \"engagement-lab-home\" CMS,', done => {
         request.get('/cms/@/home/').end((err, res) => {
             expect(res.statusCode).to.equal(200);
-            // expect(res.type).to.be.a('string', 'text/html');
             done(err);
         });
     });
+
     it('CMS logout route redirects server root,', done => {
         request.get('/cms/logout').end((err, res) => {
             expect(res.statusCode).to.equal(302);
