0938447625f8e625ee02bf86beffbcf252ab87a9
Johnny Richardson <johnny.c.rich@gmail.com>
2020-05-28 14:18:29 -0400

    fix(shortener): fixes for URL copying, success alert.

--- packages/shortener/client/src/List.js
+++ packages/shortener/client/src/List.js
@@ -4,11 +4,12 @@ import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
+import { Snackbar, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Tooltip } from '@material-ui/core';
 
-import { FileCopyOutlined } from "@material-ui/icons";
+import { FileCopyOutlined, Link, LaunchOutlined } from "@material-ui/icons";
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const GET_LINKS = gql`
     {
@@ -22,48 +23,58 @@ const GET_LINKS = gql`
 `;
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-  shortLabel:  {
-      color: 'grey',
-      fontSize: 'x-small',
-  }
+    table: {
+        minWidth: 650,
+    },
+    shortLabel:  {
+        color: 'grey',
+        fontSize: 'x-small',
+    },
+    invisible: {
+        display: 'inline-block',
+        color: 'white',
+        width: 0,
+    }
 });
 
-/**
- * Copy URL to clipboard
- * @function
- * @param {Element} [el] - DOM element containing URL to copy to clipboard
- */
-const copyUrl = (el) => {
 
-    // Create a range and set it to the contents of the reference element
-    const range = document.createRange();  
-    range.selectNodeContents(el);
-
-    window.getSelection().addRange(range);
-    const copied = document.execCommand('copy');
-
-    window.getSelection().removeRange(range);
+function Alert(props) {
+    return <MuiAlert icon={<Link fontSize="inherit" />} elevation={6} variant="filled" {...props} />;
 }
-
 
 export default function DenseTable() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
+  const [copied, setCopied] = React.useState(false);
   const { loading, error, data } = useQuery(GET_LINKS);
+
+    /**
+     * Copy URL to clipboard
+     * @function
+     * @param {Element} [el] - DOM element containing URL to copy to clipboard
+     */
+    const copyUrl = (el) => {
+
+        // Create a range and set it to the contents of the reference element
+        const range = document.createRange();  
+        range.selectNodeContents(el);
+
+        window.getSelection().addRange(range);
+        
+        const didCopy = document.execCommand('copy');
+        window.getSelection().removeRange(range);
+
+        setCopied(didCopy);
+
+    }
+
+    const snackbarClose = (event, reason) => {
+
+        setCopied(false);
+
+    };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <h2><p>Error :(</p><p>{JSON.stringify(error)}</p></h2>;
-  
-  const handleTooltipClose = () => {
-    setOpen(false);
-  };
-
-  const handleTooltipOpen = () => {
-    setOpen(true);
-  };
 
   return (
      <TableContainer component={Paper}>
@@ -73,7 +84,7 @@ export default function DenseTable() {
                  <TableRow>
                      <TableCell>Label</TableCell>
                      <TableCell>Short URL <span className={classes.shortLabel}>elab.works/...</span></TableCell>
-                     <TableCell>Original URL</TableCell>
+                     <TableCell>Original URL <LaunchOutlined size="small" /></TableCell>
                      <TableCell align="right">Clicks</TableCell>
                  </TableRow>
              </TableHead>
@@ -85,20 +96,24 @@ export default function DenseTable() {
                          {row.label}
                      </TableCell>
                      <TableCell>
-                         <Tooltip title="Add" arrow onClose={handleTooltipClose} open={open} disableFocusListener
-                             disableHoverListener disableTouchListener>
-                             <IconButton aria-label="copy url" onClick={e=>
-                                 {copyUrl(document.getElementById(`label-${row.id}`)); handleTooltipOpen();}}>
-                                 <FileCopyOutlined />
-                             </IconButton>
-                         </Tooltip>
+                        <IconButton aria-label="copy url" onClick={e=>
+                            { copyUrl(document.getElementById(`hidden-${row.id}`)); }
+                        }>
+                            <FileCopyOutlined />
+                        </IconButton>
+                         <span id={`hidden-${row.id}`} className={classes.invisible}>https://elab.works/{row.shortUrl}</span>
                          <span id={`label-${row.id}`}>{row.shortUrl}</span>
                      </TableCell>
-                     <TableCell>{row.originalUrl}</TableCell>
+                     <TableCell><a href={row.originalUrl} target="_blank">{row.originalUrl}</a></TableCell>
                      <TableCell align="right">{row.clicks}</TableCell>
                  </TableRow>
                  ))}
-             </TableBody>
+             </TableBody> 
+            <Snackbar open={copied} autoHideDuration={1000} onClose={snackbarClose}>
+                <Alert severity="success">
+                    Copied
+                </Alert>
+            </Snackbar>
          </Table>
      </TableContainer>
   );
