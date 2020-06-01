2714eb09fb5ac287bf1a035de99d8e2bd6daea33
Johnny Richardson <johnny.c.rich@gmail.com>
2020-06-01 14:25:54 -0400

    refactor(core): package entry now toggles between 'server' mode, which serves data APIs and CMS, or  just exposing utilities.

--- packages/core/app.js
+++ packages/core/app.js
@@ -11,6 +11,40 @@
  *
  * ==========
  */
+const colors = require('colors');
+
+// Base API server
 const server = require('./server');
 
-module.exports = server();
+// Create logger
+require('./logger');
+
+/*
+ * If "server" mode is enabled, run core express server,
+ * which serves all data APIs and CMS instances.
+ * Otherwise, just expose core utilities.
+ */
+if (process.env.SERVER_MODE === true || process.argv.indexOf('--server') > -1)
+    server();
+else {
+    /**
+     * @typedef {Object} Utils
+     * @property {Object} Auth - Authentication middleware
+     * @property {function} Passport - Passport configuration middleware
+     */
+    /**
+     * Engagement Lab Content and Data API Core Utilities
+     * @type Utils - Core Utilities
+     */
+    module.exports = {
+        // eslint-disable-next-line global-require
+        Auth: require('./utils/auth'),
+        // eslint-disable-next-line global-require
+        Passport: require('./utils/passport'),
+    };
+    global.logger.info(
+        `${colors.bgGreen.bold.blue('Core')}: Utils ready for use. (${
+            process.env.NODE_ENV === 'development' ? 'Development' : 'Production'
+        } Mode).`
+    );
+}
