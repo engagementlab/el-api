ae1f766b7bd4cff887d60fe5c688efa3b336e30e
Johnny Richardson <johnny.c.rich@gmail.com>
2020-10-22 17:46:39 -0400

    feat: pm2 ecosystem config.

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        script: 'packages/core/index.js --server',
+        watch: '.',
+    }],
+
+    deploy: {
+        production: {
+            user: 'node',
+            host: '199.94.80.32',
+            ref: 'origin/master',
+            repo: 'https://github.com/engagementlab/el-api',
+            path: '/srv/el-api',
+            'post-deploy': 'yarn run bootstrap && pm2 reload ecosystem.config.js --env production',
+        },
+    },
+};
