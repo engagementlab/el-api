f8f78d63aa58d6cab6c2defe505119c8a33adc7a
Johnny Richardson <johnny.c.rich@gmail.com>
2020-10-23 14:31:19 -0400

    fix: pm2 ecosystem fix.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     apps: [{
         name: 'el-api',
-        script: 'npm start',
+        script: 'npm',
+        args: ['run', 'start'],
         exec_interpreter: '/home/node/.nvm/versions/node/v14.14.0/bin/node',
         env_: {"NODE_ENV": "production",},
     }],
@@ -13,7 +14,7 @@ module.exports = {
             ref: 'origin/master',
             repo: 'https://github.com/engagementlab/el-api',
             path: '/srv/el-api',
-            'post-setup': 'nvm use && npm i -g lerna yarn && yarn run bootstrap && pm2 start ecosystem.config.js',
+            'post-setup': 'nvm use && npm i -g lerna yarn && yarn run bootstrap && yarn run build && pm2 start ecosystem.config.js',
             'post-deploy': 'nvm use && yarn run bootstrap && pm2 reload ecosystem.config.js --env production',
         },
     },
