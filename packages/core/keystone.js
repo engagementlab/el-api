38beb0d04a8f25e0e4d50a037b199e1a3081debd
Johnny Richardson <johnny.c.rich@gmail.com>
2021-02-26 16:52:44 -0500

    refactor(core): revert most changes done to mount only one package to allow schemas to mount in prod.

--- packages/core/keystone.js
+++ packages/core/keystone.js
@@ -29,9 +29,21 @@ const cloudinaryAdapter = new CloudinaryAdapter({
 });
 const apiPath = '/ql';
 
+/*
+        The API backend for CMS needs all possible lists in-memory,
+        so we have to load all models for all packages.
+
+        Create an object of all models loaded and check for duplicate names/keys,
+        as this can cause issues in keystone's list adapter global,
+        and leads to CRUD operations for one model to occur in model instantiated
+        following another.
+*/
+const allLists = {};
+
 const KeystoneApp = (ksConfig, callback) => {
-    // Retrieve info for package being mounted
-    const currentPackage = utils.GetPackagesData(false, `${ksConfig.package.schema}`)[ksConfig.package.schema];
+    const prod = process.env.NODE_ENV === 'production';
+    // Retrieve info for package(s) being mounted
+    const currentPackages = prod ? utils.GetPackagesData() : utils.GetPackagesData(false, `${ksConfig.package.schema}`);
     // Array to hold all model references
     const modelsMerged = [];
     // All admin UI apps
@@ -41,56 +53,59 @@ const KeystoneApp = (ksConfig, callback) => {
     // All schema (graphql) apps
     const schemaApps = {};
 
-    const packageInit = require(`../${currentPackage.dir}`);
-    // Load all data for API of currently used package
-    const appPackage = packageInit({
-        dbPrefix: ksConfig.dbPrefix,
-        skipRoutes: true,
-    });
+    // For all packages...
+    const keys = Object.keys(currentPackages);
+    keys.forEach(pkgKey => {
+        const packageInit = require(`../${currentPackages[pkgKey].dir}`);
 
-    // Export models, config for this app
-    const config = {
-        package: appPackage.Config,
-        models: appPackage.Models,
-    };
+        // Load all data for API of currently used package
+        const appPackage = packageInit({
+            dbPrefix: ksConfig.dbPrefix,
+            skipRoutes: true,
+        });
 
-    const schemaName = config.package.schema;
-    const dbAddress = `${ksConfig.dbPrefix}${config.package.database}?retryWrites=true&w=majority`;
+        // Export models, config for this app
+        const config = {
+            package: appPackage.Config,
+            models: appPackage.Models,
+        };
+        const schemaName = config.package.schema;
+        const dbAddress = `${ksConfig.dbPrefix}${config.package.database}?retryWrites=true&w=majority`;
+    
+        // Add instantiated model to array of all
+        config.models.forEach(modelFunc => {
+            // All models need access to cloudinary adapter
+            const modelObj = modelFunc(cloudinaryAdapter);
+            // Apply adapterName to all models based on respective schemaName
+            modelObj.adapterName = schemaName;
+            // Apply 'name' of model from it's function's name, for later key creation
+            modelObj.name = modelFunc.name;
+    
+            modelsMerged.push(modelObj);
+        });
+        
+        // Assign mongoose adapter for app
+        schemaAdapters[schemaName] = new MongooseAdapter({
+            mongoUri: dbAddress,
+        });
 
-    // Add instantiated model to array of all
-    config.models.forEach(modelFunc => {
-        // All models need access to cloudinary adapter
-        const modelObj = modelFunc(cloudinaryAdapter);
-        // Apply adapterName to all models based on respective schemaName
-        modelObj.adapterName = schemaName;
-        // Apply 'name' of model from it's function's name, for later key creation
-        modelObj.name = modelFunc.name;
-
-        modelsMerged.push(modelObj);
-    });
-
-    // Assign mongoose adapter for app
-    schemaAdapters[schemaName] = new MongooseAdapter({
-        mongoUri: dbAddress,
-    });
-
-    // Assign schema app
-    schemaApps[schemaName] = new GraphQLApp({
-        apiPath,
-        schemaName,
-    });
-
-    // Create admin UI app if on dev
-    adminApps.push(
-        new AdminUIApp({
-            // Name of CMS to load in dev instance
-            name: ksConfig.package.name,
-            adminPath: `/cms`,
-            apiPath: `/ql/?schema=${schemaName}`,
+        // Assign schema app
+        schemaApps[schemaName] = new GraphQLApp({
+            apiPath,
             schemaName,
-        })
-    );
+        });
 
+        // Create admin UI app if on dev
+        adminApps.push(
+            new AdminUIApp({
+            // Name of CMS to load in dev instance
+                name: ksConfig.package.name,
+                adminPath: `/cms`,
+                apiPath: `/ql/?schema=${schemaName}`,
+                schemaName,
+            })
+        );
+    });
     const keystone = new Keystone({
         schemaNames: Object.keys(schemaAdapters).concat(['public']),
         adapters: schemaAdapters,
@@ -98,13 +113,27 @@ const KeystoneApp = (ksConfig, callback) => {
         cookieSecret: process.env.SESSION_COOKIE,
     });
 
-    // Create list for each model
+    // Create an object of all models loaded and check for duplicate names/keys,
+    // as this can cause issues in keystone's list adapter global,
+    // and leads to CRUD operations for one model to occur in model instantiated
+    // following another
     modelsMerged.forEach(model => {
+        let thisKey = model.name;
+        // Check if model key already present in global CMS lists
+        if (Object.keys(allLists).indexOf(thisKey) > -1) {
+            // If present, mutate key w/ adaptername prefix
+            thisKey = `${model.adapterName.replace(/-/g, '')}__${thisKey}`;
+        }
+        allLists[thisKey] = model;
+    });
+
+    Object.keys(allLists).forEach(modelName => {
+        const list = allLists[modelName];
         // TODO: not hard-code and enable only on dev
-        keystone.createList(model.name, {
-            fields: model.fields,
-            ...model.options,
-            adapterName: model.adapterName,
+        keystone.createList(modelName, {
+            fields: list.fields,
+            ...list.options,
+            adapterName: list.adapterName,
         });
     });
 
