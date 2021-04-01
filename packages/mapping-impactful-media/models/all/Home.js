919a39cc4fb6285f4ddefb831df39b2a8ec33810
Johnny Richardson <johnny.c.rich@gmail.com>
2021-04-01 18:06:54 -0400

    feat(mapping-impactful-media): Home model.

---
+++ packages/mapping-impactful-media/models/all/Home.js
@@ -0,0 +1,70 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * Mapping Impactful Media CMS
+ *
+ * Home
+ * @module models
+ * @author Johnny Richardson
+ *
+ * ==========
+ */
+
+const {
+    Text,
+} = require('@keystonejs/fields');
+const { Markdown } = require('@keystonejs/fields-markdown');
+
+const {
+    CloudinaryImage,
+} = require('@keystonejs/fields-cloudinary-image');
+
+const Home = cloudinary => {
+    /**
+     * Model Fields
+     * @memberof Home
+     */
+    const fields = {
+        name: {
+            type: Text,
+            defaultValue: 'Home Page',
+            access: false,
+            isRequired: true,
+
+        },
+        intro: {
+            type: Markdown,
+            isRequired: true,
+        },
+    };
+
+    /**
+     * Model Options
+     * @memberof Home
+     * @see https://www.keystonejs.com/api/create-list
+     */
+    const options = {
+        label: 'Home Page',
+        plural: 'Home',
+        singular: 'MIM Home',
+        path: 'home',
+        listQueryName: 'MimHomePages',
+        itemQueryName: 'MimHomePage',
+        adminConfig: {
+            defaultColumns: 'label',
+        },
+        access: {
+            create: false,
+            delete: false,
+        },
+    };
+
+    return {
+        fields,
+        options,
+    };
+};
+
+/**
+ * Model Registration
+ */
+module.exports = Home;
