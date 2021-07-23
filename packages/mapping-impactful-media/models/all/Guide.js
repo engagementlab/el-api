16ba1e872f6b9c5a767a1a277b398d631a7ce8d6
Johnny Richardson <johnny.c.rich@gmail.com>
2021-07-23 18:23:28 -0400

    feat(mapping-impactful-media): Guide model.

---
+++ packages/mapping-impactful-media/models/all/Guide.js
@@ -0,0 +1,91 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/**
+ * Mapping Impactful Media CMS
+ *
+ * Guide
+ * @module models
+ * @author Johnny Richardson
+ *
+ * ==========
+ */
+
+const { Text, } = require('@keystonejs/fields');
+const { Markdown, } = require('@keystonejs/fields-markdown');
+
+const ContentRefreshHook = require('../refreshHook')
+
+const Guide = cloudinary => {
+    /**
+   * Model Fields
+   * @memberof Guide
+   */
+    const fields = {
+        name: {
+            type: Text,
+            defaultValue: 'Guide General Text',
+            access: false,
+            isRequired: true,
+        },
+        welcome: {
+            type: Markdown,
+            label: 'Welcome Text',
+            isRequired: true,
+        },
+        startText1: {
+            type: Markdown,
+            label: 'Start Text Upper',
+            isRequired: true,
+        },
+        startTextItems: {
+            type: Markdown,
+            label: 'Start Text Values',
+            isRequired: true,
+        },
+        startText2: {
+            type: Markdown,
+            label: 'Start Text Lower',
+            isRequired: true,
+        },
+        map: {
+            type: Text,
+            label: 'Map Help Text',
+            isRequired: true,
+            isMultiline: true,
+        },
+        terms: {
+            type: Text,
+            label: 'Terms Help Text',
+            isRequired: true,
+            isMultiline: true,
+        },
+    };
+
+    /**
+   * Model Options
+   * @memberof Guide
+   * @see https://www.keystonejs.com/api/create-list
+   */
+    const options = {
+        label: 'Guide General Text',
+        singular: 'Guide General Text',
+        plural: 'Guide Text',
+        path: 'guidegeneral',
+        access: {
+            delete: false,
+        },
+    };
+    const hooks = {
+        beforeChange: ContentRefreshHook,
+    };
+
+    return {
+        hooks,
+        fields,
+        options,
+    };
+};
+
+/**
+ * Model Registration
+ */
+module.exports = Guide;
